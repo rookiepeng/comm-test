764e4d086ede117044d4a88c698f42ce5792ddce
Zach Peng <rookie.dev@gmail.com>
2020-04-11 21:11:23 -0400

    add udp

--- udp.py
+++ udp.py
@@ -8,6 +8,11 @@ class UDP(QObject):
     ERROR = -1
     LISTEN = 1
     CONNECTED = 2
+    STOP = 3
+
+    SIG_NORMAL = 0
+    SIG_STOP = 1
+    SIG_DISCONNECT = 2
 
     def __init__(self, ip, port):
         QObject.__init__(self)
@@ -15,8 +20,9 @@ class UDP(QObject):
         self.ip = ip
         self.port = port
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tcp_socket.settimeout(1)
 
-        self.running = True
+        self.signal = self.SIG_NORMAL
 
     @pyqtSlot()
     def start(self):
@@ -24,28 +30,31 @@ class UDP(QObject):
             self.tcp_socket.bind((self.ip, self.port))
         except OSError as err:
             print('emit tcp server error')
-            self.status.emit(self.ERROR, '')
+            self.status.emit(self.STOP, '')
         else:
-            while self.running:
+            while True:
                 # Wait for a connection
-                print('wait for a connection')
-                self.status.emit(self.LISTEN, '')
-                try:
-                    data, addr = self.tcp_socket.recvfrom(4096)
-                except OSError as err:
-                    print(err)
-                else:
-                    if data:
-                        # self.connection.sendall(data)
-                        self.message.emit(addr, data.decode())
+                if self.signal == self.SIG_NORMAL:
+                    print('wait for a connection')
+                    self.status.emit(self.LISTEN, '')
+                    try:
+                        data, addr = self.tcp_socket.recvfrom(4096)
+                    except socket.timeout as t_out:
+                        pass
                     else:
-                        break
+                        if data:
+                            self.message.emit(addr, data.decode())
+                        else:
+                            break
+                elif self.signal == self.SIG_STOP:
+                    self.signal = self.SIG_NORMAL
+                    self.tcp_socket.close()
+                    break
+        finally:
+            self.status.emit(self.STOP, '')
 
-    def send(self, msg):
-        self.connection.sendall(msg.encode())
+    def send(self, msg, ip, port):
+        self.tcp_socket.sendto(msg.encode(), (ip, port))
 
     def close(self):
-        self.tcp_socket.close()
-        self.running = False
-        self.status.emit(self.ERROR, '')
-        print('close socket')
+        self.signal = self.SIG_STOP
