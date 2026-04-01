9de43dd2acb2ace23c9a188063be34da1a6d7f3a
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2026-04-01 08:08:05 -0400

    Refactor formatting and add network attributes
    Normalize code style and formatting (consistent double quotes, multiline class/tuple/dict layouts, cleaned file header and UI string quotes) and add network-related instance attributes (self.net_if, self.local_tcp_addr, self.local_udp_addr). update_network_interfaces now assigns to self.net_if and the new attributes store selected interface addresses. No behavioral changes beyond exposing these attributes and minor UI/style consistency fixes.

--- socket_test.py
+++ socket_test.py
@@ -1,41 +1,41 @@
 """
-    Copyright (C) 2017 - PRESENT  Zhengyu Peng, https://zpeng.me
+Copyright (C) 2017 - PRESENT  Zhengyu Peng, https://zpeng.me
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    ----------
+----------
 
-    `                      `
-    -:.                  -#:
-    -//:.              -###:
-    -////:.          -#####:
-    -/:.://:.      -###++##:
-    ..   `://:-  -###+. :##:
-           `:/+####+.   :##:
-    .::::::::/+###.     :##:
-    .////-----+##:    `:###:
-     `-//:.   :##:  `:###/.
-       `-//:. :##:`:###/.
-         `-//:+######/.
-           `-/+####/.
-             `+##+.
-              :##:
-              :##:
-              :##:
-              :##:
-              :##:
-               .+:
+`                      `
+-:.                  -#:
+-//:.              -###:
+-////:.          -#####:
+-/:.://:.      -###++##:
+..   `://:-  -###+. :##:
+       `:/+####+.   :##:
+.::::::::/+###.     :##:
+.////-----+##:    `:###:
+ `-//:.   :##:  `:###/.
+   `-//:. :##:`:###/.
+     `-//:+######/.
+       `-/+####/.
+         `+##+.
+          :##:
+          :##:
+          :##:
+          :##:
+          :##:
+           .+:
 
 """
 
@@ -54,44 +54,45 @@ from handlers import TcpHandler, UdpHandler, BluetoothHandler, CanHandler, GpibH
 from style import APP_STYLESHEET
 
 
-class MyApp(QtWidgets.QMainWindow,
-            TcpHandler, UdpHandler, BluetoothHandler, CanHandler, GpibHandler):
+class MyApp(
+    QtWidgets.QMainWindow,
+    TcpHandler,
+    UdpHandler,
+    BluetoothHandler,
+    CanHandler,
+    GpibHandler,
+):
 
     def __init__(self):
         super(MyApp, self).__init__()
 
+        self.net_if = {}
+        self.local_tcp_addr = "0.0.0.0"
+        self.local_udp_addr = "0.0.0.0"
+
         self.status = dict(
             TCP=dict(
-                Server='[SERVER] Idle',
-                Client='[CLIENT] Idle',
-                Message='[SERVER] Idle ● [CLIENT] Idle'
+                Server="[SERVER] Idle",
+                Client="[CLIENT] Idle",
+                Message="[SERVER] Idle ● [CLIENT] Idle",
             ),
-            UDP=dict(
-                Server='[SERVER] Idle',
-                Message='[SERVER] Idle'
-            ),
+            UDP=dict(Server="[SERVER] Idle", Message="[SERVER] Idle"),
             Bluetooth=dict(
-                Server='[SERVER] Idle',
-                Client='[CLIENT] Idle',
-                Message='[SERVER] Idle ● [CLIENT] Idle'
+                Server="[SERVER] Idle",
+                Client="[CLIENT] Idle",
+                Message="[SERVER] Idle ● [CLIENT] Idle",
             ),
-            CAN=dict(
-                Message='Idle'
-            ),
-            GPIB=dict(
-                Message='Idle'
-            ),
-            About=dict(
-                Message=''
-            )
+            CAN=dict(Message="Idle"),
+            GPIB=dict(Message="Idle"),
+            About=dict(Message=""),
         )
 
-        config_file = Path('config.json')
+        config_file = Path("config.json")
         if config_file.exists():
-            self.config = json.load(open('config.json', 'r'))
+            self.config = json.load(open("config.json", "r"))
         else:
             self.config = dict()
-            json.dump(self.config, open('config.json', 'w+'))
+            json.dump(self.config, open("config.json", "w+"))
 
         ui_file = QFile("mainwindow.ui")
         loader = QUiLoader()
@@ -109,20 +110,20 @@ class MyApp(QtWidgets.QMainWindow,
 
         self.ui.tabWidget.currentChanged.connect(self.on_tab_changed)
         self.ui.buttonClear.clicked.connect(self.ui.textBrowserMessage.clear)
-        self.ui.buttonClear.setToolTip('Clear message log')
+        self.ui.buttonClear.setToolTip("Clear message log")
 
         self.ui.show()
 
     def save_config(self):
         try:
-            json.dump(self.config, open('config.json', 'w+'))
+            json.dump(self.config, open("config.json", "w+"))
         except PermissionError:
             pass
 
     def init_ui(self):
         self.update_network_interfaces()
 
-        self.ui.tabWidget.setCurrentIndex(self.config.get('Tab_Index', 0))
+        self.ui.tabWidget.setCurrentIndex(self.config.get("Tab_Index", 0))
         self.on_tab_changed()
 
         self.init_tcp_ui()
@@ -143,26 +144,31 @@ class MyApp(QtWidgets.QMainWindow,
         central_layout.addWidget(splitter)
 
         # Initialize groupbox border states
-        for gb in (self.ui.groupBox_14, self.ui.groupBox_16,
-                   self.ui.groupBox_15, self.ui.groupBox_8,
-                   self.ui.groupBox_17, self.ui.groupBox_10):
-            self._set_groupbox_state(gb, 'idle')
+        for gb in (
+            self.ui.groupBox_14,
+            self.ui.groupBox_16,
+            self.ui.groupBox_15,
+            self.ui.groupBox_8,
+            self.ui.groupBox_17,
+            self.ui.groupBox_10,
+        ):
+            self._set_groupbox_state(gb, "idle")
 
     def _set_groupbox_state(self, groupbox, state):
         _COLORS = {
-            'idle': '#bdbdbd',
-            'listening': '#FF9800',
-            'connected': '#4CAF50',
-            'error': '#F44336',
+            "idle": "#bdbdbd",
+            "listening": "#FF9800",
+            "connected": "#4CAF50",
+            "error": "#F44336",
         }
-        color = _COLORS.get(state, '#bdbdbd')
+        color = _COLORS.get(state, "#bdbdbd")
         groupbox.setStyleSheet(
             f"QGroupBox {{ border: 1px solid {color}; border-radius: 4px; margin: 3px; margin-top: 10px; padding: 2px; }} "
             f"QGroupBox::title {{ subcontrol-origin: margin; left: 8px; padding: 0 4px; }}"
         )
 
     def _log_message(self, label, source, msg, incoming=False):
-        ts = datetime.now().strftime('%H:%M:%S.%f')[:-3]
+        ts = datetime.now().strftime("%H:%M:%S.%f")[:-3]
         ts_html = f'<span style="color:#90A4AE;font-size:small;">[{ts}]</span>'
         if incoming:
             self.ui.textBrowserMessage.append(
@@ -170,17 +176,17 @@ class MyApp(QtWidgets.QMainWindow,
             )
         else:
             self.ui.textBrowserMessage.append(
-                f'<div>{ts_html} <strong>&#8593; [{label}] {source}</strong><br>{msg}</div>'
+                f"<div>{ts_html} <strong>&#8593; [{label}] {source}</strong><br>{msg}</div>"
             )
 
     def update_network_interfaces(self):
         temp_net_if = psutil.net_if_addrs()
         self.net_if = psutil.net_if_addrs()
 
-        tcp_interface_idx = self.config.get('TcpInterface', 0)
+        tcp_interface_idx = self.config.get("TcpInterface", 0)
         self.ui.comboBoxTcpInterface.clear()
 
-        udp_interface_idx = self.config.get('UdpInterface', 0)
+        udp_interface_idx = self.config.get("UdpInterface", 0)
         self.ui.comboBoxUdpInterface.clear()
 
         net_if_stats = psutil.net_if_stats()
@@ -194,53 +200,53 @@ class MyApp(QtWidgets.QMainWindow,
 
         if tcp_interface_idx >= self.ui.comboBoxTcpInterface.count():
             self.ui.comboBoxTcpInterface.setCurrentIndex(0)
-            self.config['TcpInterface'] = 0
+            self.config["TcpInterface"] = 0
         else:
             self.ui.comboBoxTcpInterface.setCurrentIndex(tcp_interface_idx)
 
         if udp_interface_idx >= self.ui.comboBoxUdpInterface.count():
             self.ui.comboBoxUdpInterface.setCurrentIndex(0)
-            self.config['UdpInterface'] = 0
+            self.config["UdpInterface"] = 0
         else:
             self.ui.comboBoxUdpInterface.setCurrentIndex(udp_interface_idx)
 
-        tcp_addr = ''
+        tcp_addr = ""
         for snicaddr in self.net_if[self.ui.comboBoxTcpInterface.currentText()]:
             if snicaddr.family == socket.AF_INET:
-                tcp_addr = tcp_addr + 'IPv4: ' + snicaddr.address + ' '
+                tcp_addr = tcp_addr + "IPv4: " + snicaddr.address + " "
                 self.local_tcp_addr = snicaddr.address
                 break
             else:
-                self.local_tcp_addr = '0.0.0.0'
+                self.local_tcp_addr = "0.0.0.0"
 
         self.ui.labelTcpServerIp.setText(tcp_addr)
 
-        udp_addr = ''
+        udp_addr = ""
         for snicaddr in self.net_if[self.ui.comboBoxUdpInterface.currentText()]:
             if snicaddr.family == socket.AF_INET:
-                udp_addr = udp_addr + 'IPv4: ' + snicaddr.address + ' '
+                udp_addr = udp_addr + "IPv4: " + snicaddr.address + " "
                 self.local_udp_addr = snicaddr.address
                 break
             else:
-                self.local_udp_addr = '0.0.0.0'
+                self.local_udp_addr = "0.0.0.0"
 
         self.ui.labelUdpHostAddress.setText(udp_addr)
         self.save_config()
 
     def on_tab_changed(self):
         self.ui.status_bar.clearMessage()
-        self.ui.status_bar.setStyleSheet('color: green')
+        self.ui.status_bar.setStyleSheet("color: green")
         tab_name = self.ui.tabWidget.tabText(self.ui.tabWidget.currentIndex())
-        self.ui.status_bar.showMessage(self.status[tab_name]['Message'])
+        self.ui.status_bar.showMessage(self.status[tab_name]["Message"])
 
-        self.config['Tab_Index'] = self.ui.tabWidget.currentIndex()
+        self.config["Tab_Index"] = self.ui.tabWidget.currentIndex()
         self.save_config()
 
 
 if __name__ == "__main__":
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QtWidgets.QApplication(sys.argv)
-    app.setStyle('Fusion')
+    app.setStyle("Fusion")
     app.setStyleSheet(APP_STYLESHEET)
     window = MyApp()
     sys.exit(app.exec())
